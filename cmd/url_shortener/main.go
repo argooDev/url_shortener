67a38ba4eb65476734d8a78d02920746ca8079e0
argooDev <gorshechnikof@gmail.com>
2024-10-31 22:49:00 +0500

    configure project

---
+++ cmd/url_shortener/main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	// TODO: init config: cleanenv
+
+	// TODO: init logger: slog
+
+	// TODO: init storage: sqlite
+
+	// TODO: init router: chi  -  совместим с net/http, chi render
+
+	// TODO: run server:
+
+}
